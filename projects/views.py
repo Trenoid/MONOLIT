82096a0ecbaa110ad7d53a54e14b9cb4bc1bd0af
Trenoid <gcfhcjh@gmail.com>
2024-05-28 18:59:16 +0300

    The project filter

--- projects/views.py
+++ projects/views.py
@@ -1,13 +1,41 @@
+from math import floor
+from nis import cat
 from django.shortcuts import render
 from projects.models import Categories, Projects
 
 def catalog(request):
+
+    on_sale = request.GET.get('on_sale',None)
+    order_by = request.GET.get("order_by",None)
+
     catalog = Projects.objects.all()
+
+    if on_sale:
+        catalog = catalog.filter(discount__gt=0)
+
+    if order_by and order_by != "default":
+        catalog = catalog.order_by(order_by)
+
+ 
+    categories = []
+    for category in Categories.objects.all():
+        category_projects = catalog.filter(floor=category)
+        categories.append(
+            {
+                'category': category,
+                'projects' : category_projects,
+            }
+        )
+    
+
     context = {
         "title": "Каталог проектов",
         "catalog": catalog,
+        "categories": categories,
 
     }
+    print(f"Final catalog: {list(catalog)}")
+    print(f"categories: {categories}")
 
     return render(request, "projects/catalog.html", context)
 
--- static/filter.js
+++ static/filter.js
@@ -1,6 +1,8 @@
+
 const filterButton = document.querySelector("[data-action='filter-open']");
 const filterForm = document.getElementById("filter-form");
 
 filterButton.addEventListener("click", (event) => {
   filterForm.hidden = !filterForm.hidden;
+  console.log("Форма фильтрации отображается: ", !filterForm.hidden);
 });
