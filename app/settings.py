451a2c35c56e4560ed0b28caa0c58c438761cb93
Trenoid <gcfhcjh@gmail.com>
2024-05-28 23:24:51 +0300

    postgresql connected

--- app/settings.py
+++ app/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "monolith",
+        'USER' : "monolith",
+        'PASSWORD' : "monolith23",
+        'HOST' : "localhost",
+        'PORT' : "5432",
     }
 }
 
