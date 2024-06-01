eefde860c8836628156b805b44aadba994fc334f
Trenoid <gcfhcjh@gmail.com>
2024-06-01 22:18:18 +0300

    add model user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+
+    RECOMMENDATION_STATUS_CHOICES = [
+        ('silver', 'Серебро'),
+        ('gold', 'Золото'),
+        ('platinum', 'Платинум'),
+    ]
+
+    recommendation_status = models.CharField(
+        max_length=10,
+        choices=RECOMMENDATION_STATUS_CHOICES,
+        default='silver'
+    )
+    download_link = models.URLField(blank=True, null=True)
+    project_completion_percentage = models.PositiveIntegerField(default=0)
+    his_referral_code = models.CharField(max_length=10, blank=True, null=True)
+    referral_code_that_gives_him_a_discount = ...
+
+
+    class Meta:
+        db_table = "user"
+        verbose_name = "Пользователя"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.username
