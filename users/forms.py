31500919e2abe004e9564d741bac1615f66492f1
Trenoid <gcfhcjh@gmail.com>
2024-06-02 16:11:56 +0300

    redact form for login

--- users/forms.py
+++ users/forms.py
@@ -4,5 +4,28 @@ from django.contrib.auth.forms import AuthenticationForm
 from users.models import User
 
 class UserLoginForm(AuthenticationForm):
+
+
+    username = forms.CharField()
+    password = forms.CharField()
+
+
+
+
+    # username = forms.CharField(
+    #     label = "username",
+    #     widget=forms.TextInput(attrs={"autofocus": True,
+    #                                   "class" : "authorization__input",
+    #                                   "placeholder": "Имя пользователя"},))
+    
+    # password = forms.CharField(
+    #     label = "password",
+    #     widget=forms.PasswordInput(attrs={"autocomplete": "current-password",
+    #                                       "class" : "authorization__input",
+    #                                       "placeholder": "Пароль"
+    #                                       }),
+    # )
+
     class Meta:
         model = User
+        fields = ["username","password",]
--- users/models.py
+++ users/models.py
@@ -8,17 +8,31 @@ class User(AbstractUser):
         ('gold', 'Золото'),
         ('platinum', 'Платинум'),
     ]
+    PROJECT_COMPLETION_PERCENTAGE_CHOICES = [
+        (0,0),
+        (25,25),
+        (50,50),
+        (100,100),
+    ]
 
     recommendation_status = models.CharField(
         max_length=10,
         choices=RECOMMENDATION_STATUS_CHOICES,
-        default='silver'
+        default='silver',
+        verbose_name= "Статус рекомендации"
     )
-    download_link = models.URLField(blank=True, null=True)
-    project_completion_percentage = models.PositiveIntegerField(default=0)
-    his_referral_code = models.CharField(max_length=10, blank=True, null=True)
-    referral_code_that_gives_him_a_discount = ...
+    download_link = models.URLField(blank=True, null=True,verbose_name="Ссылка на скачивание")
+    project_completion_percentage = models.PositiveIntegerField(default=0,
+                                                                choices= PROJECT_COMPLETION_PERCENTAGE_CHOICES, 
+                                                                verbose_name="Процент завершенности проекта")
+    
 
+    balance = models.DecimalField(default = 0, max_digits = 9 , decimal_places = 2, verbose_name = "Баланс пользователя")
+
+    phone = models.CharField(max_length=20, blank=True, null=True, verbose_name= "Номер телефона")
+    city = models.CharField(max_length=100, blank=True, null=True, verbose_name= "город")
+    referal_code = models.CharField(max_length=30,blank=True,null=True,verbose_name="Реферальный код")
+    referal_code_used = models.PositiveIntegerField(default=0, verbose_name= "Число использований реферального кода пользователя")
 
     class Meta:
         db_table = "user"
